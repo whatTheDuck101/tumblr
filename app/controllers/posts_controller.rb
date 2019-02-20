ab69c9a696816f1013a9c560ad63ec3ae226dfa9
Elizabeth Creary <baebaebun101@gmail.com>
2019-02-20 15:45:43 -0500

    validations, form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,15 @@ class PostsController < ApplicationController
     @posts = Post.all.order('created_at DESC')
   end
   def new
-    @oost = Post.new
+    @post = Post.new
   end
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
   def show
   end
@@ -23,19 +26,19 @@ class PostsController < ApplicationController
     else
       render 'edit'
     end
-  end 
+  end
 
   def destroy
     @post.destroy
+    redirect_to root_path
   end
-
 
   private
 
   def post_params
     params.require(:post).permit(:title, :body)
   end
-  def post_find 
+  def post_find
     @post = Post.find(params[:id])
   end 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 50 }
-  validates :body, presence: true
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 50 }
 end
