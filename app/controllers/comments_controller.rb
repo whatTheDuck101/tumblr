8bcb4699f8cb68562d978bbdde17f23f88c6a22a
Elizabeth Creary <baebaebun101@gmail.com>
2019-02-22 14:29:01 -0500

    create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,20 @@
 class CommentsController < ApplicationController
-  before_action :post_comment_find, only: [:create, :destroy, :edit, :update]
+  before_action :post_find, only: [:create, :destroy]
+
   def create
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.create(params[:comment].permit(:name, :body))
     redirect_to post_path(@post)
   end
 
-  def edit 
-  end
-  def update 
-  end
-
   def destroy
-    @comment.destory
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
     redirect_to post_path(@post)
   end
 
   private
-  def post_comment_find 
+
+  def post_find
     @post = Post.find(params[:post_id])
-    @comment = @post.comment.find(params[:id])
-
-  end
-
-  def comment_params
-    params.require[:comment].permit(:name, :body)
   end
 end
