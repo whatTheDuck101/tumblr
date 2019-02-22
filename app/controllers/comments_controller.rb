bea21c7a376d3e7d8754e83a28295505cc1e3f47
Elizabeth Creary <baebaebun101@gmail.com>
2019-02-22 12:30:47 -0500

    delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,14 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comment.find(params[:id])
+    @comment.destory
+    
+    redirect_to post_path(@post)
+  end
+
   private
   def comment_params
     params.require[:comment].permit(:name, :body)
